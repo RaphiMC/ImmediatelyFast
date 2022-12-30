815ec5b143f6d37eb925317fb47a3cd72e33fc6c
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2022-12-30 01:54:55 +0100

    Fixed incompatibility with owo-lib

--- src/main/java/net/raphimc/immediatelyfast/injection/mixins/batching/consumer/MixinDrawableHelper.java
+++ src/main/java/net/raphimc/immediatelyfast/injection/mixins/batching/consumer/MixinDrawableHelper.java
@@ -2,35 +2,33 @@ package net.raphimc.immediatelyfast.injection.mixins.batching.consumer;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.client.render.*;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.util.math.ColorHelper;
 import net.minecraft.util.math.Matrix4f;
 import net.raphimc.immediatelyfast.feature.batching.BatchingBuffers;
 import net.raphimc.immediatelyfast.feature.batching.BatchingRenderLayers;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = DrawableHelper.class, priority = 900)
+@Mixin(value = DrawableHelper.class, priority = 1500)
 public abstract class MixinDrawableHelper {
 
-    /**
-     * @author RK_01
-     * @reason Allow batching of the vertex data. Overwritten for performance.
-     */
-    @Overwrite
-    public static void fill(Matrix4f matrix, int x1, int y1, int x2, int y2, int color) {
-        if (x1 < x2) {
-            x1 = x1 ^ x2;
-            x2 = x1 ^ x2;
-            x1 = x1 ^ x2;
-        }
-        if (y1 < y2) {
-            y1 = y1 ^ y2;
-            y2 = y1 ^ y2;
-            y1 = y1 ^ y2;
-        }
-
+    @Inject(method = "fill(Lnet/minecraft/util/math/Matrix4f;IIIII)V", at = @At("HEAD"), cancellable = true)
+    private static void fillIntoBuffer(Matrix4f matrix, int x1, int y1, int x2, int y2, int color, CallbackInfo ci) {
         if (BatchingBuffers.FILL_CONSUMER != null) {
+            ci.cancel();
+            if (x1 < x2) {
+                x1 = x1 ^ x2;
+                x2 = x1 ^ x2;
+                x1 = x1 ^ x2;
+            }
+            if (y1 < y2) {
+                y1 = y1 ^ y2;
+                y2 = y1 ^ y2;
+                y1 = y1 ^ y2;
+            }
             final float[] shaderColor = RenderSystem.getShaderColor();
             final int argb = (int) (shaderColor[3] * 255) << 24 | (int) (shaderColor[0] * 255) << 16 | (int) (shaderColor[1] * 255) << 8 | (int) (shaderColor[2] * 255);
             color = ColorHelper.Argb.mixColor(color, argb);
@@ -39,30 +37,13 @@ public abstract class MixinDrawableHelper {
             vertexConsumer.vertex(matrix, x2, y2, 0F).color(color).next();
             vertexConsumer.vertex(matrix, x2, y1, 0F).color(color).next();
             vertexConsumer.vertex(matrix, x1, y1, 0F).color(color).next();
-        } else {
-            RenderSystem.enableBlend();
-            RenderSystem.disableTexture();
-            RenderSystem.defaultBlendFunc();
-            RenderSystem.setShader(GameRenderer::getPositionColorShader);
-            final BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
-            bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
-            bufferBuilder.vertex(matrix, x1, y2, 0F).color(color).next();
-            bufferBuilder.vertex(matrix, x2, y2, 0F).color(color).next();
-            bufferBuilder.vertex(matrix, x2, y1, 0F).color(color).next();
-            bufferBuilder.vertex(matrix, x1, y1, 0F).color(color).next();
-            BufferRenderer.drawWithShader(bufferBuilder.end());
-            RenderSystem.enableTexture();
-            RenderSystem.disableBlend();
         }
     }
 
-    /**
-     * @author RK_01
-     * @reason Allow batching of the vertex data. Overwritten for performance.
-     */
-    @Overwrite
-    public static void drawTexturedQuad(Matrix4f matrix, int x0, int x1, int y0, int y1, int z, float u0, float u1, float v0, float v1) {
+    @Inject(method = "drawTexturedQuad", at = @At("HEAD"), cancellable = true)
+    private static void drawTexturedQuadIntoBuffer(Matrix4f matrix, int x0, int x1, int y0, int y1, int z, float u0, float u1, float v0, float v1, CallbackInfo ci) {
         if (BatchingBuffers.TEXTURE_CONSUMER != null) {
+            ci.cancel();
             final float[] shaderColor = RenderSystem.getShaderColor();
             final int r = (int) (shaderColor[0] * 255);
             final int g = (int) (shaderColor[1] * 255);
@@ -73,15 +54,6 @@ public abstract class MixinDrawableHelper {
             vertexConsumer.vertex(matrix, x1, y1, z).texture(u1, v1).color(r, g, b, a).next();
             vertexConsumer.vertex(matrix, x1, y0, z).texture(u1, v0).color(r, g, b, a).next();
             vertexConsumer.vertex(matrix, x0, y0, z).texture(u0, v0).color(r, g, b, a).next();
-        } else {
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            final BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
-            bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
-            bufferBuilder.vertex(matrix, x0, y1, z).texture(u0, v1).next();
-            bufferBuilder.vertex(matrix, x1, y1, z).texture(u1, v1).next();
-            bufferBuilder.vertex(matrix, x1, y0, z).texture(u1, v0).next();
-            bufferBuilder.vertex(matrix, x0, y0, z).texture(u0, v0).next();
-            BufferRenderer.drawWithShader(bufferBuilder.end());
         }
     }
 
--- src/main/java/net/raphimc/immediatelyfast/injection/mixins/batching/consumer/MixinTextRenderer.java
+++ src/main/java/net/raphimc/immediatelyfast/injection/mixins/batching/consumer/MixinTextRenderer.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(value = TextRenderer.class, priority = 1100)
+@Mixin(value = TextRenderer.class, priority = 1500)
 public abstract class MixinTextRenderer {
 
     @ModifyArg(method = "draw(Ljava/lang/String;FFILnet/minecraft/util/math/Matrix4f;ZZ)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Ljava/lang/String;FFIZLnet/minecraft/util/math/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;ZIIZ)I"))
