198637102e180e2f2050c7d84f9f4275c235e67b
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-01-11 21:03:17 +0100

    Updated copyright headers

--- src/main/java/net/raphimc/immediatelyfast/injection/mixins/map_atlas_generation/MixinMapRenderer.java
+++ src/main/java/net/raphimc/immediatelyfast/injection/mixins/map_atlas_generation/MixinMapRenderer.java
@@ -2,10 +2,10 @@
  * This file is part of ImmediatelyFast - https://github.com/RaphiMC/ImmediatelyFast
  * Copyright (C) 2023 RK_01/RaphiMC and contributors
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
