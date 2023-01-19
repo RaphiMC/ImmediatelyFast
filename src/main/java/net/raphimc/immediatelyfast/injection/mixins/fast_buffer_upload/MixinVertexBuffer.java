f872e626d93710b3534a08bd11549d7ff3597591
Fluffy Bumblebee <lromanic123alt@gmail.com>
2023-01-19 17:20:52 +1100

    idek what im doing

--- src/main/java/net/raphimc/immediatelyfast/injection/mixins/fast_buffer_upload/MixinVertexBuffer.java
+++ src/main/java/net/raphimc/immediatelyfast/injection/mixins/fast_buffer_upload/MixinVertexBuffer.java
@@ -17,43 +17,85 @@
  */
 package net.raphimc.immediatelyfast.injection.mixins.fast_buffer_upload;
 
+import com.mojang.blaze3d.platform.GlConst;
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gl.VertexBuffer;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.VertexFormat;
+import net.raphimc.immediatelyfast.injection.interfaces.IVertexFormat;
+import org.jetbrains.annotations.Nullable;
 import org.lwjgl.opengl.GL15C;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.nio.ByteBuffer;
 
 @Mixin(value = VertexBuffer.class, priority = 500)
 public abstract class MixinVertexBuffer {
+    @Shadow private VertexFormat vertexFormat;
+    @Shadow public abstract VertexFormat getVertexFormat();
+    @Shadow private int vertexBufferId;
+    @Shadow private int indexBufferId;
+    @Shadow private VertexFormat.DrawMode drawMode;
+    @Shadow private int vertexCount;
+    @Unique private int indexBufferSize;
 
-    @Unique
-    private int vertexBufferSize;
 
-    @Unique
-    private int indexBufferSize;
-
-    @Redirect(method = "configureVertexFormat", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;glBufferData(ILjava/nio/ByteBuffer;I)V"))
-    private void optimizeVertexDataUploading(int target, ByteBuffer data, int usage) {
-        if (data.remaining() > this.vertexBufferSize) {
-            this.vertexBufferSize = data.remaining();
-            RenderSystem.glBufferData(target, data, GL15C.GL_DYNAMIC_DRAW);
-        } else {
-            GL15C.glBufferSubData(target, 0, data);
-        }
+    @Inject(method = "uploadInternal", at = @At(target = "Lnet/minecraft/client/render" +
+            "/BufferBuilder$DrawArrayParameters;getVertexCount()I", value = "INVOKE"))
+    private void replace_value_format(BufferBuilder buffer, CallbackInfo ci) {
+        vertexFormat = configureVertexFormat(buffer.popData().getFirst(), buffer.popData().getSecond());
     }
 
-    @Redirect(method = "configureIndexBuffer", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;glBufferData(ILjava/nio/ByteBuffer;I)V"))
-    private void optimizeIndexDataUploading(int target, ByteBuffer data, int usage) {
-        if (data.remaining() > this.indexBufferSize) {
-            this.indexBufferSize = data.remaining();
-            RenderSystem.glBufferData(target, data, GL15C.GL_DYNAMIC_DRAW);
-        } else {
-            GL15C.glBufferSubData(target, 0, data);
+
+    private VertexFormat configureVertexFormat(BufferBuilder.DrawArrayParameters parameters, ByteBuffer data) {
+        boolean bl = false;
+        if (!parameters.getVertexFormat().equals(this.getVertexFormat())) {
+            if (this.getVertexFormat() != null) {
+                ((IVertexFormat)vertexFormat).clearState();
+            }
+
+            GlStateManager._glBindBuffer(GlConst.GL_ARRAY_BUFFER, this.vertexBufferId);
+            ((IVertexFormat)parameters.getVertexFormat()).setUpState();
+            bl = true;
         }
+
+        if (parameters.hasNoIndexBuffer()) {
+            if (!bl) {
+                GlStateManager._glBindBuffer(GlConst.GL_ARRAY_BUFFER, this.vertexBufferId);
+            }
+
+            if (data.remaining() > this.indexBufferSize) {
+                this.indexBufferSize = data.remaining();
+                RenderSystem.glBufferData(GlConst.GL_ARRAY_BUFFER, data, GL15C.GL_DYNAMIC_DRAW);
+            } else {
+                GL15C.glBufferSubData(GlConst.GL_ARRAY_BUFFER, 0, data);
+            }
+        }
+
+        return parameters.getVertexFormat();
     }
 
+    /*@Nullable
+    private RenderSystem.IndexBuffer configureIndexBuffer(BufferBuilder.DrawArrayParameters parameters, ByteBuffer data) {
+        if (!parameters.hasNoIndexBuffer()) {
+            GlStateManager._glBindBuffer(GlConst.GL_ELEMENT_ARRAY_BUFFER, this.indexBufferId);
+            RenderSystem.glBufferData(GlConst.GL_ELEMENT_ARRAY_BUFFER, data, GlConst.GL_STATIC_DRAW);
+            return null;
+        } else {
+            RenderSystem.IndexBuffer indexBuffer = RenderSystem.getSequentialBuffer(parameters.getMode(),
+                    parameters.getCount());
+            if (indexBuffer != RenderSystem.getSequentialBuffer(this.drawMode, this.vertexCount) || !indexBuffer.isSizeLessThanOrEqual(parameters.getCount())) {
+                indexBuffer.bindAndGrow(parameters.getCount());
+            }
+
+            return indexBuffer;
+        }
+    }*/
 }
