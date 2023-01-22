eebd792b23a661ef04f8835c100993b11634ae43
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-01-22 19:58:56 +0100

    Added comments to config to explain the config sections

--- src/main/java/net/raphimc/immediatelyfast/feature/core/ImmediatelyFastConfig.java
+++ src/main/java/net/raphimc/immediatelyfast/feature/core/ImmediatelyFastConfig.java
@@ -20,6 +20,7 @@ package net.raphimc.immediatelyfast.feature.core;
 public class ImmediatelyFastConfig {
 
     // Regular config values
+    private String REGULAR_INFO = "----- Regular config values below -----";
     public boolean font_atlas_resizing = true;
     public boolean map_atlas_generation = true;
     public boolean hud_batching = true;
@@ -27,12 +28,15 @@ public class ImmediatelyFastConfig {
     public boolean fast_buffer_upload = true;
 
     // Cosmetic config values
+    private String COSMETIC_INFO = "----- Cosmetic only config values below (Does not optimize anything) -----";
     public boolean dont_add_info_into_debug_hud = false;
 
     // Experimental config values
+    private String EXPERIMENTAL_INFO = "----- Experimental config values below (Rendering glitches may occur) -----";
     public boolean experimental_item_hud_batching = false;
 
     // Debug config values
+    private String DEBUG_INFO = "----- Debug only config values below (Do not touch) -----";
     public boolean debug_only_and_not_recommended_disable_universal_batching = false;
 
 }
