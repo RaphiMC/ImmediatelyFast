eaca0084e7e66f70e92da97570ad0ddb714e5bb1
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-02-17 12:42:05 +0100

    Fixed depth test issues

--- src/main/java/net/raphimc/immediatelyfast/feature/batching/BlendFuncDepthFunc.java
+++ src/main/java/net/raphimc/immediatelyfast/feature/batching/BlendFuncDepthFunc.java
@@ -21,10 +21,11 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import org.lwjgl.opengl.GL11C;
 import org.lwjgl.opengl.GL14C;
 
-public record BlendFuncDepthFunc(int GL_BLEND_SRC_RGB, int GL_BLEND_SRC_ALPHA, int GL_BLEND_DST_RGB, int GL_BLEND_DST_ALPHA, int GL_DEPTH_FUNC) {
+public record BlendFuncDepthFunc(boolean DEPTH_TEST, int GL_BLEND_SRC_RGB, int GL_BLEND_SRC_ALPHA, int GL_BLEND_DST_RGB, int GL_BLEND_DST_ALPHA, int GL_DEPTH_FUNC) {
 
     public static BlendFuncDepthFunc current() {
         return new BlendFuncDepthFunc(
+                GL11C.glGetBoolean(GL14C.GL_DEPTH_TEST),
                 GL11C.glGetInteger(GL14C.GL_BLEND_SRC_RGB),
                 GL11C.glGetInteger(GL14C.GL_BLEND_SRC_ALPHA),
                 GL11C.glGetInteger(GL14C.GL_BLEND_DST_RGB),
@@ -35,7 +36,12 @@ public record BlendFuncDepthFunc(int GL_BLEND_SRC_RGB, int GL_BLEND_SRC_ALPHA, i
 
     public void apply() {
         RenderSystem.blendFuncSeparate(GL_BLEND_SRC_RGB, GL_BLEND_DST_RGB, GL_BLEND_SRC_ALPHA, GL_BLEND_DST_ALPHA);
-        RenderSystem.depthFunc(GL_DEPTH_FUNC);
+        if (DEPTH_TEST) {
+            RenderSystem.enableDepthTest();
+            RenderSystem.depthFunc(GL_DEPTH_FUNC);
+        } else {
+            RenderSystem.disableDepthTest();
+        }
     }
 
 }
