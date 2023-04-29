17d146dc62713c33de2de745f022b48adc9740aa
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-04-29 13:15:31 +0200

    Fixed build error

--- src/main/java/net/raphimc/immediatelyfast/injection/mixins/hud_batching/compat/armorchroma/MixinArmorChroma_GuiArmor.java
+++ src/main/java/net/raphimc/immediatelyfast/injection/mixins/hud_batching/compat/armorchroma/MixinArmorChroma_GuiArmor.java
@@ -20,6 +20,7 @@ package net.raphimc.immediatelyfast.injection.mixins.hud_batching.compat.armorch
 import net.raphimc.immediatelyfast.ImmediatelyFast;
 import net.raphimc.immediatelyfast.feature.batching.BatchingBuffers;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,6 +28,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @SuppressWarnings("UnresolvedMixinReference")
 @Mixin(targets = "nukeduck.armorchroma.GuiArmor", remap = false)
+@Pseudo
 public abstract class MixinArmorChroma_GuiArmor {
 
     @Unique
