ebff7d0c33c420c21e680e33766a28fc1d8e41b5
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-09-06 23:12:46 +0200

    Fixed HUD batching on Forge not doing anything

--- common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/hud_batching/compat/appleskin/MixinAppleSkin_HUDOverlayHandler.java
+++ common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/hud_batching/compat/appleskin/MixinAppleSkin_HUDOverlayHandler.java
@@ -17,34 +17,24 @@
  */
 package net.raphimc.immediatelyfast.injection.mixins.hud_batching.compat.appleskin;
 
-import net.minecraft.client.gui.hud.InGameHud;
 import net.raphimc.immediatelyfast.ImmediatelyFast;
 import net.raphimc.immediatelyfast.feature.batching.BatchingBuffers;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = InGameHud.class, priority = 1500)
-public abstract class MixinInGameHud {
+@SuppressWarnings("UnresolvedMixinReference")
+@Mixin(targets = "squeek.appleskin.client.HUDOverlayHandler", remap = false)
+@Pseudo
+public abstract class MixinAppleSkin_HUDOverlayHandler {
 
-    @Unique
-    private boolean wasHudBatching;
-
-    @Inject(method = "renderStatusBars", at = @At(value = "CONSTANT", args = "stringValue=food", shift = At.Shift.BEFORE))
-    private void endHudBatching(CallbackInfo ci) {
+    @Inject(method = "drawExhaustionOverlay", at = @At("RETURN"))
+    private void forceDrawBatch(CallbackInfo ci) {
         if (ImmediatelyFast.runtimeConfig.hud_batching && BatchingBuffers.isHudBatching()) {
             BatchingBuffers.endHudBatching();
-            this.wasHudBatching = true;
-        }
-    }
-
-    @Inject(method = "renderStatusBars", at = @At(value = "CONSTANT", args = "stringValue=food", shift = At.Shift.BY, by = 2))
-    private void beginHudBatching(CallbackInfo ci) {
-        if (this.wasHudBatching) {
             BatchingBuffers.beginHudBatching();
-            this.wasHudBatching = false;
         }
     }
 
