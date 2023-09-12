f4569b934f3f79f37cbfd0c1242d235e2f7731c8
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-09-12 18:10:37 +0200

    Fixed text translucency issues on Forge

--- fabric/src/main/java/net/raphimc/immediatelyfast/fabric/injection/mixins/core/MixinRenderLayer.java
+++ fabric/src/main/java/net/raphimc/immediatelyfast/fabric/injection/mixins/core/MixinRenderLayer.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.raphimc.immediatelyfast.injection.mixins.core;
+package net.raphimc.immediatelyfast.fabric.injection.mixins.core;
 
 import net.minecraft.client.render.RenderLayer;
 import org.spongepowered.asm.mixin.Mixin;
