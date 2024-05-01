cbae441c774c0793c94a1aab46a9298dd89333a5
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2024-05-01 13:12:15 +0200

    Draw current batch when mods render stuff on their own Fixes #197

---
+++ common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/hud_batching/MixinRenderSystem.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of ImmediatelyFast - https://github.com/RaphiMC/ImmediatelyFast
+ * Copyright (C) 2023-2024 RK_01/RaphiMC and contributors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.raphimc.immediatelyfast.injection.mixins.hud_batching;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.raphimc.immediatelyfast.feature.batching.BatchingBuffers;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RenderSystem.class)
+public abstract class MixinRenderSystem {
+
+    @Inject(method = "drawElements", at = @At("HEAD"))
+    private static void checkForDrawCallWhileBatching(CallbackInfo ci) {
+        if (BatchingBuffers.FILL_CONSUMER != null) {
+            // If some mod tries to directly draw something while we are batching, we should end the current batch and start a new one, so that the draw order is correct.
+            BatchingBuffers.endHudBatching();
+            BatchingBuffers.beginHudBatching();
+        }
+    }
+
+}
