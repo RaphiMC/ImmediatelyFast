5f02ebb1303c4bcafecd5cdcbf9738a574588ac3
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2024-05-03 01:43:03 +0200

    Changed mixin priorities

--- common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/hud_batching/MixinInGameHud.java
+++ common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/hud_batching/MixinInGameHud.java
@@ -26,7 +26,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = InGameHud.class, priority = 1500)
+@Mixin(value = InGameHud.class, priority = 500)
 public abstract class MixinInGameHud {
 
     @Inject(method = {
