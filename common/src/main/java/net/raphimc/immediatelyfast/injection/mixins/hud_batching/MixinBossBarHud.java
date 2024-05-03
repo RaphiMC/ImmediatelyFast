d89bbf39fb81b0c0a272984ad4fb56181b39a4bc
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2024-05-03 11:29:32 +0200

    Fixed more injection ordering issues

--- common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/hud_batching/MixinBossBarHud.java
+++ common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/hud_batching/MixinBossBarHud.java
@@ -20,15 +20,17 @@ package net.raphimc.immediatelyfast.injection.mixins.hud_batching;
 import net.minecraft.client.gui.hud.BossBarHud;
 import net.raphimc.immediatelyfast.ImmediatelyFast;
 import net.raphimc.immediatelyfast.feature.batching.BatchingBuffers;
+import net.raphimc.immediatelyfast.injection.processors.InjectAboveEverything;
 import net.raphimc.immediatelyfast.injection.processors.InjectOnAllReturns;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = BossBarHud.class, priority = 500)
+@Mixin(value = BossBarHud.class, priority = 1500)
 public abstract class MixinBossBarHud {
 
+    @InjectAboveEverything
     @Inject(method = "render", at = @At("HEAD"))
     private void beginBatching(CallbackInfo ci) {
         if (ImmediatelyFast.runtimeConfig.hud_batching) {
--- common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/screen_batching/MixinChatScreen.java
+++ common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/screen_batching/MixinChatScreen.java
@@ -19,15 +19,17 @@ package net.raphimc.immediatelyfast.injection.mixins.screen_batching;
 
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.raphimc.immediatelyfast.feature.batching.BatchingBuffers;
+import net.raphimc.immediatelyfast.injection.processors.InjectAboveEverything;
 import net.raphimc.immediatelyfast.injection.processors.InjectOnAllReturns;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = ChatScreen.class, priority = 500)
+@Mixin(value = ChatScreen.class, priority = 1500)
 public abstract class MixinChatScreen {
 
+    @InjectAboveEverything
     @Inject(method = "render", at = @At("HEAD"))
     private void beginBatching(CallbackInfo ci) {
         BatchingBuffers.beginHudBatching();
