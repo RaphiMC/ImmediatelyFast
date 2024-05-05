77f7924c540d37dc8212bc2636bbe9ce27ade4d4
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2024-05-05 20:22:47 +0200

    Track texture 1 and 2 in RenderSystemState

--- common/src/main/java/net/raphimc/immediatelyfast/feature/batching/RenderSystemState.java
+++ common/src/main/java/net/raphimc/immediatelyfast/feature/batching/RenderSystemState.java
@@ -20,19 +20,23 @@ package net.raphimc.immediatelyfast.feature.batching;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.render.Shader;
 
-public record RenderSystemState(int texture0, Shader program, float[] shaderColor, BlendFuncDepthFunc blendFuncDepthFunc) {
+public record RenderSystemState(int texture0, int texture1, int texture2, Shader program, float[] shaderColor, BlendFuncDepthFunc blendFuncDepthFunc) {
 
     public static RenderSystemState current() {
         return new RenderSystemState(
                 RenderSystem.getShaderTexture(0),
+                RenderSystem.getShaderTexture(1),
+                RenderSystem.getShaderTexture(2),
                 RenderSystem.getShader(),
                 RenderSystem.getShaderColor().clone(),
-                net.raphimc.immediatelyfast.feature.batching.BlendFuncDepthFunc.current()
+                BlendFuncDepthFunc.current()
         );
     }
 
     public void apply() {
         RenderSystem.setShaderTexture(0, this.texture0);
+        RenderSystem.setShaderTexture(1, this.texture1);
+        RenderSystem.setShaderTexture(2, this.texture2);
         RenderSystem.setShader(() -> this.program);
         RenderSystem.setShaderColor(this.shaderColor[0], this.shaderColor[1], this.shaderColor[2], this.shaderColor[3]);
         this.blendFuncDepthFunc.apply();
