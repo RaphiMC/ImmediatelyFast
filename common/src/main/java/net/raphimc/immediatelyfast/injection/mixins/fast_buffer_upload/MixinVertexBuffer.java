9ad1b0e5f0a74bcf2821632ada4d95735386e755
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2024-12-15 18:14:34 +0100

    Don't use fast buffer upload for static buffers
    Fixes OpenGL performance warning on AMD GPUs

--- common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/fast_buffer_upload/MixinVertexBuffer.java
+++ common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/fast_buffer_upload/MixinVertexBuffer.java
@@ -18,18 +18,25 @@
 package net.raphimc.immediatelyfast.injection.mixins.fast_buffer_upload;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import net.minecraft.client.gl.GlUsage;
 import net.minecraft.client.gl.GpuBuffer;
 import net.minecraft.client.gl.VertexBuffer;
 import net.raphimc.immediatelyfast.ImmediatelyFast;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(VertexBuffer.class)
 public abstract class MixinVertexBuffer {
 
+    @Shadow
+    @Final
+    private GlUsage usage;
+
     @WrapWithCondition(method = "uploadVertexBuffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/GpuBuffer;resize(I)V"))
     private boolean onlyResizeIfNeeded(GpuBuffer instance, int newSize) {
-        return !ImmediatelyFast.runtimeConfig.fast_buffer_upload || newSize > instance.size;
+        return !ImmediatelyFast.runtimeConfig.fast_buffer_upload || this.usage == GlUsage.STATIC_WRITE || newSize > instance.size;
     }
 
 }
