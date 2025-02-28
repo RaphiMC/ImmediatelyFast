4736b5dd89068af1e40053d5dad8e4decb417a27
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2025-02-28 18:17:17 +0100

    Cleaned up code

--- common/src/main/java/net/raphimc/immediatelyfast/apiimpl/BatchingAccessImpl.java
+++ common/src/main/java/net/raphimc/immediatelyfast/apiimpl/BatchingAccessImpl.java
@@ -60,7 +60,7 @@ public class BatchingAccessImpl implements BatchingAccess {
     private void warn() {
         if (!this.warned) {
             this.warned = true;
-            ImmediatelyFast.LOGGER.error("A mod tried to use the ImmediatelyFast batching API, but it is no longer available in 1.21.2.");
+            ImmediatelyFast.LOGGER.error("A mod tried to use the ImmediatelyFast batching API, but it is no longer available in 1.21.");
             ImmediatelyFast.LOGGER.error("Mojang added basic batching into the DrawContext class. ImmediatelyFast now uses and extends this system, so this method is no longer needed.");
             ImmediatelyFast.LOGGER.error("To migrate your mod, simply remove all calls to the ImmediatelyFast batching API and make sure to use the DrawContext for your HUD rendering.");
             ImmediatelyFast.LOGGER.error("Here is a stack trace to help you find the offending code:", new Exception());
