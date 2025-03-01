c3e945ef01b9f2000b353c304558615e49865e35
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2025-03-01 22:58:07 +0100

    Fixed HUD batching graphical issues on NeoForge

---
+++ neoforge/src/main/java/net/raphimc/immediatelyfast/neoforge/injection/mixins/hud_batching/MixinGuiLayerManager.java
@@ -0,0 +1,47 @@
+/*
+ * This file is part of ImmediatelyFast - https://github.com/RaphiMC/ImmediatelyFast
+ * Copyright (C) 2023-2025 RK_01/RaphiMC and contributors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.raphimc.immediatelyfast.neoforge.injection.mixins.hud_batching;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderTickCounter;
+import net.neoforged.neoforge.client.gui.GuiLayerManager;
+import net.raphimc.immediatelyfast.ImmediatelyFast;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@SuppressWarnings("UnstableApiUsage")
+@Mixin(GuiLayerManager.class)
+public abstract class MixinGuiLayerManager {
+
+    @Inject(
+            method = "renderInner",
+            at = @At(value = "INVOKE", target = "Lnet/neoforged/bus/api/IEventBus;post(Lnet/neoforged/bus/api/Event;)Lnet/neoforged/bus/api/Event;", shift = At.Shift.AFTER, remap = false),
+            slice = @Slice(
+                    from = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/LayeredDrawer$Layer;render(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/render/RenderTickCounter;)V")
+            )
+    )
+    private void renderBatch(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        if (ImmediatelyFast.runtimeConfig.hud_batching && !ImmediatelyFast.config.experimental_universal_hud_batching) {
+            context.draw();
+        }
+    }
+
+}
