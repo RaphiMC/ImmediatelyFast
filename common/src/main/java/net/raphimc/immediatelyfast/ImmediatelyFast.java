7d07943d2e233e173d1a61d50318c764f910edfa
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2025-03-03 23:45:26 +0100

    Removed Scorched Guns 2 from the HUD batching disable list
    The previous commit also fixes the graphical issues with Scorched Guns 2 while also keeping HUD batching enabled

--- common/src/main/java/net/raphimc/immediatelyfast/ImmediatelyFast.java
+++ common/src/main/java/net/raphimc/immediatelyfast/ImmediatelyFast.java
@@ -55,11 +55,7 @@ public class ImmediatelyFast {
 
         if (!config.debug_only_and_not_recommended_disable_mod_conflict_handling) {
             if (config.hud_batching) {
-                if (PlatformCode.getModVersion("scguns").isPresent()) {
-                    // https://github.com/ribs498/Scorched-Guns-1.20.1/issues/51
-                    LOGGER.warn("Scorched Guns 2 mod detected. Force disabling HUD Batching optimization.");
-                    config.hud_batching = false;
-                } else if (PlatformCode.getModVersion("mbd2").isPresent()) {
+                if (PlatformCode.getModVersion("mbd2").isPresent()) {
                     // https://github.com/Low-Drag-MC/Multiblocked2/issues/80
                     LOGGER.warn("Multiblocked2 mod detected. Force disabling HUD Batching optimization.");
                     config.hud_batching = false;
