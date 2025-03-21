c47fd6f7800ba2d24044c0752907fd5d1bbdd93b
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2025-03-21 20:12:33 +0100

    Simplified code

--- common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/core/MixinGlDebug.java
+++ common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/core/MixinGlDebug.java
@@ -19,8 +19,6 @@ package net.raphimc.immediatelyfast.injection.mixins.core;
 
 import net.minecraft.client.gl.GlDebug;
 import net.raphimc.immediatelyfast.ImmediatelyFast;
-import org.lwjgl.opengl.GL;
-import org.lwjgl.opengl.GLCapabilities;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -36,8 +34,7 @@ public abstract class MixinGlDebug {
 
     @ModifyVariable(method = "enableDebug", at = @At("HEAD"), index = 1, argsOnly = true)
     private static boolean enableSyncDebug(boolean sync) {
-        final GLCapabilities capabilities = GL.getCapabilities();
-        return sync || (ImmediatelyFast.config.debug_only_print_additional_error_information && (capabilities.GL_KHR_debug || capabilities.GL_ARB_debug_output));
+        return sync || ImmediatelyFast.config.debug_only_print_additional_error_information;
     }
 
     @Redirect(method = "info", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;info(Ljava/lang/String;Ljava/lang/Object;)V", remap = false))
