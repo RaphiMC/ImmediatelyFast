e299f2ba257a688581f076217f1e4f5847bac535
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2025-03-26 16:45:19 +0100

    Readded NeoForge support

--- neoforge/src/main/java/net/raphimc/immediatelyfast/neoforge/injection/mixins/core/MixinNeoForgeRenderTypes.java
+++ neoforge/src/main/java/net/raphimc/immediatelyfast/neoforge/injection/mixins/core/MixinNeoForgeRenderTypes.java
@@ -31,7 +31,7 @@ public abstract class MixinNeoForgeRenderTypes {
             "getTextIntensityPolygonOffsetFiltered",
             "getTextSeeThroughFiltered",
             "getTextIntensitySeeThroughFiltered"
-    }, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/RenderLayer;of(Ljava/lang/String;Lnet/minecraft/client/render/VertexFormat;Lnet/minecraft/client/render/VertexFormat$DrawMode;IZZLnet/minecraft/client/render/RenderLayer$MultiPhaseParameters;)Lnet/minecraft/client/render/RenderLayer$MultiPhase;"), index = 5)
+    }, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/RenderLayer;of(Ljava/lang/String;IZZLcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/client/render/RenderLayer$MultiPhaseParameters;)Lnet/minecraft/client/render/RenderLayer$MultiPhase;"), index = 3)
     private static boolean changeTranslucency(boolean value) {
         return false;
     }
