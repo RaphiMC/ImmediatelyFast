35311de88c7a2654ecf0244eb1aafcbc49e17f11
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2025-04-07 00:07:22 +0200

    Unbind framebuffer before swapping buffers
    Fixes #350

---
+++ common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/avoid_redundant_framebuffer_switching/MixinRenderSystem.java
@@ -0,0 +1,37 @@
+/*
+ * This file is part of ImmediatelyFast - https://github.com/RaphiMC/ImmediatelyFast
+ * Copyright (C) 2023-2025 RK_01/RaphiMC and contributors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.raphimc.immediatelyfast.injection.mixins.avoid_redundant_framebuffer_switching;
+
+import com.mojang.blaze3d.opengl.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import org.lwjgl.opengl.GL30C;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RenderSystem.class)
+public abstract class MixinRenderSystem {
+
+    @Inject(method = "flipFrame", at = @At("HEAD"))
+    private static void unbindFramebufferBeforeSwappingBuffers(CallbackInfo ci) {
+        // https://github.com/RaphiMC/ImmediatelyFast/issues/350
+        GlStateManager._glBindFramebuffer(GL30C.GL_FRAMEBUFFER, 0);
+    }
+
+}
