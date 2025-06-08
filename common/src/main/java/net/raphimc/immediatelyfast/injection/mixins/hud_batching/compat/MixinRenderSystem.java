64a3661ea7de581fc86f9ce10ee2cc66ed4435d0
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2025-06-08 14:58:11 +0200

    Force draw HUD batch when the projection matrix is modified
    Fixes #376

---
+++ common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/hud_batching/compat/MixinRenderSystem.java
@@ -0,0 +1,38 @@
+/*
+ * This file is part of ImmediatelyFast - https://github.com/RaphiMC/ImmediatelyFast
+ * Copyright (C) 2023-2025 RK_01/RaphiMC and contributors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.raphimc.immediatelyfast.injection.mixins.hud_batching.compat;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.raphimc.immediatelyfast.feature.batching.BatchingBuffers;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = RenderSystem.class, remap = false)
+public abstract class MixinRenderSystem {
+
+    @Inject(method = {"backupProjectionMatrix", "restoreProjectionMatrix"}, at = @At("HEAD"))
+    private static void checkForModificationWhileBatching(CallbackInfo ci) {
+        if (BatchingBuffers.isHudBatching()) {
+            // If some mod tries to modify the projection matrix while we are batching, we should end the current batch and start a new one, so that the result is correct.
+            BatchingBuffers.tryForceDrawHudBuffers();
+        }
+    }
+
+}
