130897fefb8b989a9033ec7d91f05cccbc6e648f
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2026-01-25 23:26:19 +0100

    Fixed core shader incompatibility detection logic bug
    Fixes #400

--- common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/core/compat/MixinShaderLoader.java
+++ common/src/main/java/net/raphimc/immediatelyfast/injection/mixins/core/compat/MixinShaderLoader.java
@@ -94,27 +94,36 @@ public abstract class MixinShaderLoader {
             ImmediatelyFast.LOGGER.error("Failed to check for core shader modifications", e);
         }
 
-        if (ImmediatelyFast.runtimeConfig.font_atlas_resizing && resourcePackWhichBreaksFontAtlasResizing != null) {
-            ImmediatelyFast.LOGGER.warn("Resource pack " + resourcePackWhichBreaksFontAtlasResizing.getId() + " is not compatible with font atlas resizing. Temporarily disabling font atlas resizing.");
-            ImmediatelyFast.runtimeConfig.font_atlas_resizing = false;
-            this.immediatelyFast$reloadFontStorages();
-        } else {
-            if (!ImmediatelyFast.runtimeConfig.font_atlas_resizing && ImmediatelyFast.config.font_atlas_resizing) {
+        if (ImmediatelyFast.config.font_atlas_resizing) {
+            if (resourcePackWhichBreaksFontAtlasResizing != null) {
+                ImmediatelyFast.LOGGER.warn("Resource pack " + resourcePackWhichBreaksFontAtlasResizing.getId() + " is not compatible with font atlas resizing. Temporarily disabling font atlas resizing.");
+                if (ImmediatelyFast.runtimeConfig.font_atlas_resizing) {
+                    ImmediatelyFast.runtimeConfig.font_atlas_resizing = false;
+                    this.immediatelyFast$reloadFontStorages();
+                }
+            } else if (!ImmediatelyFast.runtimeConfig.font_atlas_resizing) {
+                ImmediatelyFast.LOGGER.info("Re-enabling font atlas resizing because no incompatible resource packs are loaded.");
                 ImmediatelyFast.runtimeConfig.font_atlas_resizing = true;
                 this.immediatelyFast$reloadFontStorages();
             }
         }
-        if (ImmediatelyFast.runtimeConfig.hud_batching && resourcePackWhichBreaksHudBatching != null) {
-            ImmediatelyFast.LOGGER.warn("Resource pack " + resourcePackWhichBreaksHudBatching.getId() + " is not compatible with HUD batching. Temporarily disabling HUD batching.");
-            ImmediatelyFast.runtimeConfig.hud_batching = false;
-        } else {
-            ImmediatelyFast.runtimeConfig.hud_batching = ImmediatelyFast.config.hud_batching;
+        if (ImmediatelyFast.config.hud_batching) {
+            if (resourcePackWhichBreaksHudBatching != null) {
+                ImmediatelyFast.LOGGER.warn("Resource pack " + resourcePackWhichBreaksHudBatching.getId() + " is not compatible with HUD batching. Temporarily disabling HUD batching.");
+                ImmediatelyFast.runtimeConfig.hud_batching = false;
+            } else if (!ImmediatelyFast.runtimeConfig.hud_batching) {
+                ImmediatelyFast.LOGGER.info("Re-enabling HUD batching because no incompatible resource packs are loaded.");
+                ImmediatelyFast.runtimeConfig.hud_batching = true;
+            }
         }
-        if (ImmediatelyFast.runtimeConfig.experimental_screen_batching && resourcePackWhichBreaksScreenBatching != null) {
-            ImmediatelyFast.LOGGER.warn("Resource pack " + resourcePackWhichBreaksScreenBatching.getId() + " is not compatible with experimental screen batching. Temporarily disabling experimental screen batching.");
-            ImmediatelyFast.runtimeConfig.experimental_screen_batching = false;
-        } else {
-            ImmediatelyFast.runtimeConfig.experimental_screen_batching = ImmediatelyFast.config.experimental_screen_batching;
+        if (ImmediatelyFast.config.experimental_screen_batching) {
+            if (resourcePackWhichBreaksScreenBatching != null) {
+                ImmediatelyFast.LOGGER.warn("Resource pack " + resourcePackWhichBreaksScreenBatching.getId() + " is not compatible with experimental screen batching. Temporarily disabling experimental screen batching.");
+                ImmediatelyFast.runtimeConfig.experimental_screen_batching = false;
+            } else if (!ImmediatelyFast.runtimeConfig.experimental_screen_batching) {
+                ImmediatelyFast.LOGGER.info("Re-enabling experimental screen batching because no incompatible resource packs are loaded.");
+                ImmediatelyFast.runtimeConfig.experimental_screen_batching = true;
+            }
         }
     }
 
